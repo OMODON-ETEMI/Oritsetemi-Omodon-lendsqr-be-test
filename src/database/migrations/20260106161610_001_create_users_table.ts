aae14f332692c4153d088661f0c4f1754158bcff
etemi <etemiomodon@gmail.com>
2026-01-09 22:20:42 +0100

    chore: update user schema migration and custom database error handling

--- src/database/migrations/20260106161610_001_create_users_table.ts
+++ src/database/migrations/20260106161610_001_create_users_table.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable('users', (table) => {
-        table.uuid('id').primary().defaultTo(knex.raw('(UUID())'));
+        table.uuid('id').primary()
         table.string('first_name',100).notNullable();
         table.string('last_name',100).notNullable();
         table.string('email', 225).notNullable().unique();
